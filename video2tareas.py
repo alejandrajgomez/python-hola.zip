16dfe484ffab0992e228d47ea35830bccce8f538
Alejandra Judith Gomez <35463644+alejandrajgomez@users.noreply.github.com>
2022-05-08 21:32:14 -0300

    VIDEO 2 TAREAS1-2

---
+++ video2tareas.py
@@ -0,0 +1,7 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+# Este ejercicio lo tienes que realizar desde la consola de python y subir capturas de pantalla en una carpeta
+
+
+cadenatxt = " Esta es mi cadena"
+print(cadenatxt)
+print(type(cadenatxt))
