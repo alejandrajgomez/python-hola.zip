c2a215c495ee9da03d2e0935a1597246de118b9c
Alejandra Judith Gomez <35463644+alejandrajgomez@users.noreply.github.com>
2022-05-28 20:26:15 -0300

    VIDEO 8 TAREA 2
    guardar objeto en fichero.bin y recuperarlo

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-# En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
+# En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo, haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
 
-f = open('nombresfemeninos.txt','w')
 
-lista = ['Peponia\n',
-         'Manila\n',
-         'Roselda\n'
-       ]
+import pickle
 
-f.writelines(lista)
+class Vehiculo:
+    marca = " "
+    modelo = 0
+    color = " "
+
+    def __init__(self,marca,modelo,color):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+
+    def __str__(self):
+        return f'La marca es {self.marca} el modelo es {self.modelo} y es de color {self.color}'
+
+v1 = Vehiculo("Volvo",1970,"rojo")
+
+print(str(v1))
+
+
+f = open('vehiculo.bin','wb')
+pickle.dump(v1,f)
 f.close()
-print(lista)
 
-
-f = open('nombresfemeninos.txt','a')
-f.write('Esta es la segunda apertura del archivo, y le agrego nombres:\n')
-f.write('Rosa\n')
-f.write('Juana\n')
-f.write('Mercedes\n')
+f = open('vehiculo.bin', 'rb')# con esto lo leo
+vehiculo = pickle.load(f)
 f.close()
-
-f = open('nombresfemeninos.txt')
-print(f.read())
+print(type(vehiculo))
+print("Marca: ",vehiculo.marca, "Modelo: ",vehiculo.modelo, "Color: ",vehiculo.color)
